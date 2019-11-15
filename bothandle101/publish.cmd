695655c76435262ac23cbf2c9c05b5c684da2845
Bhattarai <subhattarai@pvamu.edu>
2019-11-15 16:02:00 -0600

    First Commit with default stuff from Azure Bot Service

---
+++ bothandle101/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=pvamubothandle101-Web-Deploy.pubxml -p:Password=dmxMfNMtsgzinheN8tPmzrsMDSnr9zosuRCQ5SFXF0w7EKldEe58M3i7TALR
+
